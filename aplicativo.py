09b48ac06063da9b4627621f98437fc2ea7ee4c6
Ni154 <niedson.santosperola@gmail.com>
2025-06-20 15:47:00 -0300

    Update aplicativo.py

--- aplicativo.py
+++ aplicativo.py
@@ -822,7 +822,7 @@ CREATE TABLE IF NOT EXISTS empresa (
 )
 """)
 conn.commit()
-   elif menu == "Cadastro Empresa":
+elif menu == "Cadastro Empresa":
     st.title("🏢 Cadastro da Empresa")
 
     empresa = cursor.execute("SELECT * FROM empresa WHERE id = 1").fetchone()
@@ -833,6 +833,7 @@ conn.commit()
         telefone = st.text_input("Telefone", value=empresa[4] if empresa else "")
         email = st.text_input("E-mail", value=empresa[5] if empresa else "")
         if st.form_submit_button("Salvar dados"):
+            
             if empresa:
                 cursor.execute("""
                     UPDATE empresa SET nome=?, cnpj=?, endereco=?, telefone=?, email=? WHERE id=1
