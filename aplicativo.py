4cfe0564b224c7bf43b1b480d0921820c26884d5
Ni154 <niedson.santosperola@gmail.com>
2025-07-09 13:06:57 -0300

    Update aplicativo.py

--- aplicativo.py
+++ aplicativo.py
@@ -1086,13 +1086,11 @@ else:
         st.success(f"Venda {venda_id} cancelada com sucesso!")
 
 
-    # --- MENU BACKUP ---
+         # --- MENU BACKUP ---
     elif menu == "Backup":
-       st.subheader("💾 Backup do Banco de Dados")
-    
-       if st.button("Fazer Backup"):
-           fazer_backup()
-    
+        st.subheader("💾 Backup do Banco de Dados")
+        fazer_backup() 
+
     # --- MENU SAIR ---
     elif menu == "Sair":
         st.session_state.login = False
