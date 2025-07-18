097851aed79663e2078b7553dd516a5d34165431
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 19:51:45 -0300

    Update servico_service.py

--- backend/models/servico_service.py
+++ backend/models/servico_service.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, String, Text, Float
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
+from config.database import Base
+
+class Servico(Base):
+    __tablename__ = "servicos"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, index=True)
+    nome = Column(String(100), nullable=False)
+    descricao = Column(Text, nullable=True)
+    preco = Column(Float, nullable=False)
 
