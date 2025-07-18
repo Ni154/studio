ef35c52b3fd9915093bbb34f23fb493736d14564
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 17:41:04 -0300

    Update database.py

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,1 +1,11 @@
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        host="postgres.railway.internal",
+        dbname="railway",
+        user="postgres",
+        password="dhuudOCvDKVgQokLiWCWtpYIMDFMiAql",
+        port=5432
+    )
 
