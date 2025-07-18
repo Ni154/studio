be7f7ac67dfae15eff5f5bd67e24f36abf54b177
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 08:58:16 -0300

    Update supabase_client.py

--- config/supabase_client.py
+++ config/supabase_client.py
@@ -1,1 +1,8 @@
+# config/supabase_client.py
+from supabase import create_client, Client
+
+# URL e Chave do Supabase (substitua pelas suas se quiser trocar depois)
+url: str = "https://zgpfvrwsjsbsvevflypk.supabase.co"
+key: str = "sb_secret_VpH-6yfypCG-7cUS6ZnQgQ_hQgPxLdJ"
+supabase: Client = create_client(url, key)
 
