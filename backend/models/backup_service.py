b981c5fea6930592e78261ecbbba4507bdb856c3
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 20:18:51 -0300

    Update backup_service.py

--- backend/models/backup_service.py
+++ backend/models/backup_service.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float
+from config.database import Base
+
+class Servico(Base):
+    __tablename__ = "servicos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, unique=True, index=True, nullable=False)
+    descricao = Column(String, nullable=True)
+    preco = Column(Float, nullable=False)
 
