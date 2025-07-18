6549b244e1734094b4db17737c3642caa15acea9
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 19:30:13 -0300

    Update cliente_service.py

--- backend/models/cliente_service.py
+++ backend/models/cliente_service.py
@@ -1,1 +1,18 @@
+from sqlalchemy import Column, Integer, String, Date
+from config.database import Base
+
+class Cliente(Base):
+    __tablename__ = "clientes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    telefone = Column(String, nullable=True)
+    email = Column(String, nullable=True)
+    nascimento = Column(Date, nullable=True)
+
+    # Ficha de anamnese
+    sintomas = Column(String, nullable=True)
+    alergias = Column(String, nullable=True)
+    medicacao = Column(String, nullable=True)
+    observacoes = Column(String, nullable=True)
 
