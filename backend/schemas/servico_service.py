be670f122d59e305dc6119030b9e73eaee3a0f61
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 19:51:01 -0300

    Update servico_service.py

--- backend/schemas/servico_service.py
+++ backend/schemas/servico_service.py
@@ -1,1 +1,18 @@
+from pydantic import BaseModel
+from uuid import UUID
+from typing import Optional
+
+class ServicoBase(BaseModel):
+    nome: str
+    descricao: Optional[str] = None
+    preco: float
+
+class ServicoCreate(ServicoBase):
+    pass
+
+class ServicoResponse(ServicoBase):
+    id: UUID
+
+    class Config:
+        orm_mode = True
 
