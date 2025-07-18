2683071956f32d586530f4bae013d7c6dde6b798
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 19:09:29 -0300

    Delete utils directory

--- utils/form_helpers.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def limpar_formulario(form_key: str):
-    """Limpa os inputs de um formulário Streamlit identificando por key"""
-    for key in st.session_state.keys():
-        if key.startswith(form_key):
-            del st.session_state[key]
