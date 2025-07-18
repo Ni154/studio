ae08bec671cff1163f08a49f7ab6df2b2f5e2cd9
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 09:31:57 -0300

    Update supabase_client.py

--- config/supabase_client.py
+++ config/supabase_client.py
@@ -1,8 +1,7 @@
-# config/supabase_client.py
-from supabase import create_client, Client
+from supabase import create_client
+import os
 
-# URL e Chave do Supabase (substitua pelas suas se quiser trocar depois)
-url: str = "https://zgpfvrwsjsbsvevflypk.supabase.co"
-key: str = "sb_secret_VpH-6yfypCG-7cUS6ZnQgQ_hQgPxLdJ"
-supabase: Client = create_client(url, key)
+SUPABASE_URL = os.getenv("SUPABASE_URL", "https://zgpfvrwsjsbsvevflypk.supabase.co")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "sb_secret_VpH-6yfypCG-7cUS6ZnQgQ_hQgPxLdJ")
 
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
