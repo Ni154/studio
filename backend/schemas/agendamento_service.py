ddf46c01e9d06ce0de8b679c46644ccc90158a2d
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 19:55:30 -0300

    Update agendamento_service.py

--- backend/schemas/agendamento_service.py
+++ backend/schemas/agendamento_service.py
@@ -1,1 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+
+class AgendamentoBase(BaseModel):
+    cliente_id: int
+    cliente_nome: str
+    data: date
+    hora: str
+    servicos: str
+    status: str = Field(default="Agendado")
+
+class AgendamentoCreate(AgendamentoBase):
+    pass
+
+class AgendamentoUpdate(BaseModel):
+    data: Optional[date]
+    hora: Optional[str]
+    servicos: Optional[str]
+    status: Optional[str]
+
+class AgendamentoInDB(AgendamentoBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
