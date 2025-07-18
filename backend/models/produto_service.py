68a0c9f3fe44b8a20a7ef341f87714a699c8740d
Ni154 <niedson.santosperola@gmail.com>
2025-07-18 19:49:28 -0300

    Update produto_service.py

--- backend/models/produto_service.py
+++ backend/models/produto_service.py
@@ -1,1 +1,15 @@
 
+from sqlalchemy import Column, String, Float, Integer
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from config.database import Base
+
+class Produto(Base):
+    __tablename__ = "produtos"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    nome = Column(String, nullable=False)
+    descricao = Column(String)
+    preco = Column(Float, nullable=False)
+    categoria = Column(String)
+    estoque = Column(Integer, default=0)
